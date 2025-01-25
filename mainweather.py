3b5c70abebdb1817328239563c9c867cdbe50303
xaybarner <xaybarner@gmail.com>
2025-01-24 21:35:17 -0800

    python weather app final draft
    final draft (minor appearance changes from Windows to Mac)

--- mainweather.py
+++ mainweather.py
@@ -126,7 +126,38 @@ class WeatherApp(QWidget):
     def display_weather(self, data):
         temperature_k = data["main"]["temp"]
         temperature_f = (temperature_k * 9/5) - 459.67
-        print(temperature_f)
+        weather_id = data["weather"][0]["id"]
+        weather_description = data["weather"][0]["description"]
+
+        self.temperature_label.setText(f"{temperature_f:.0f}°F")
+        self.emoji_label.setText(self.get_weather_emoji(weather_id))
+        self.description_label.setText(weather_description)
+
+    @staticmethod
+    def get_weather_emoji(weather_id):
+
+        if 200 <= weather_id <= 232:
+            return "⛈️"
+        elif 300 <= weather_id <= 321:
+            return "🌦️"
+        elif 500 <= weather_id <= 531:
+            return "☔️"
+        elif 600 <= weather_id <= 622:
+            return "❄️"
+        elif 701 <= weather_id <= 741:
+            return "🌬️"
+        elif weather_id == 762:
+            return "🔥"
+        elif weather_id == 771:
+            return "💨"
+        elif weather_id == 781:
+            return "🌪️"
+        elif weather_id == 800:
+            return "🌞️"
+        elif 801 <= weather_id <= 804:
+            return "☁️"
+        else:
+            return ""
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
